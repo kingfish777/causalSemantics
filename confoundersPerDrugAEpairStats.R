b494386c62e559c19f47732535e3e6e378752fb9
kingfish777 <scott.malec@gmail.com>
2020-11-08 23:17:43 -0500

    adding confounder stats histogram2

--- confoundersPerDrugAEpairStats.R
+++ confoundersPerDrugAEpairStats.R
@@ -1,6 +1,6 @@
 
 library(ggpubr)
-setwd("/Users/scottalexandermalec/Projects/enlil")
+#setwd("/Users/scottalexandermalec/Projects/enlil")
 #http://www.sthda.com/english/articles/24-ggpubr-publication-ready-plots
 confounderLists <- list.files(path = "sqlrawOriginal/")
 confounderStats <- c()
@@ -19,7 +19,9 @@ median(confounderStats)
 mean(confounderStats)
 min(confounderStats)
 max(confounderStats)
+postscript(file = "boxplots/confounderHistogram.eps", title = "confounders per drug / adverse event pair", width = 480, height = 480)
 hist(confounderStats, breaks = 200)
+dev.off()
 confounderStats
 
 
