4be64460f2eb3fecea69769688232838b3c30390
Kingfish Threesevens <kingfish777@users.noreply.github.com>
2020-11-09 22:11:06 -0500

    Update elucidatingConfoundersResults.R

--- elucidatingConfoundersResults.R
+++ elucidatingConfoundersResults.R
@@ -1,5 +1,3 @@
-
-
 library(MLmetrics)
 library(pROC)
 library(PRROC)
@@ -840,7 +838,7 @@ getGGBoxplotRawcoefs <- function(dat, hoiname) {
   plotname <- paste("Regression coefficients (β) [combined]  for ", hoiname, sep = "")
   tiff(file = fn, height = 12, width = 17, units = 'cm', compression = "lzw", res = 300)
   p <- ggboxplot(subset(dat, dat$hoiname == hoiname), title = plotname, 
-                 x = "casecontrol", y =  c("medCoef0", "medCoef", "sql5medcoef1", "sql10medcoef1", "psi5medcoef1", "psi10medcoef1"), #, "sql10medcoef1", "psi5medcoef1", "psi10medcoef1"),
+                 x = "casecontrol", y = c("medCoef0", "medCoef", "sql5medcoef1", "sql10medcoef1", "psi5medcoef1", "psi10medcoef1"), #, "sql10medcoef1", "psi5medcoef1", "psi10medcoef1"),
                  color = "casecontrol", palette = c("#00AFBB", "#E7B800"), 
                  ylab = "Exposure coefficients (β)", xlab = "casecontrol", bxp.errorbar = TRUE, 
                  combine = TRUE, add = c("mean_ci", "dotplot"), 
@@ -871,7 +869,7 @@ getGGBoxplotRawATE<- function(dat, hoiname) {
   plotname <- paste("Average Treatment Effect (Δ)  [combined] for ", hoiname, sep = "")
   tiff(file = fn, height = 12, width = 17, units = 'cm', compression = "lzw", res = 300)
   p <- ggboxplot(subset(dat, dat$hoiname == hoiname), title = plotname, 
-                 x = "casecontrol", y = c("exactATE", "sql5ate", "psi10ate"), #y = c("exactATE", "sql5ate", "sql10ate", "psi5ate", "psi10ate"), 
+                 x = "casecontrol", y = c("exactATE", "sql5ate", "sql10ate", "psi5ate", "psi10ate"), 
                  color = "casecontrol", palette = c("#00AFBB", "#E7B800"), 
                  ylab = "ATE (Δ)", xlab = "casecontrol", bxp.errorbar = TRUE, 
                  combine = TRUE, add = c("mean_ci", "dotplot"), 
@@ -900,7 +898,7 @@ combinedDat$coefDiffpsi10 <- combinedDat$medCoef0 - combinedDat$psi10medcoef1
 
 getGGBoxplotRawATE<- function(dat, hoi) {
   fn <- paste("boxplots/", hoi, "_4_coefDiff.tiff", sep = "")  
-  plotname <- paste("Difference in β [combinedCoefDiff] for ", hoiname, sep = "")
+  plotname <- paste("Difference in β [combinedCoefDiff] for ", hoi, sep = "")
   tiff(file = fn, height = 12, width = 17, units = 'cm', compression = "lzw", res = 300)
   p <- ggboxplot(subset(dat, dat$hoiname == hoi), title = plotname, 
                  x = "casecontrol", y = c("coefDiffRandom", "coefDiffsql5", "coefDiffsql10", "coefDiffpsi5", "coefDiffpsi10"), 
@@ -1450,7 +1448,3 @@ round(MLmetrics::PRAUC(
   y_true = as.factor(randomGIB$casecontrol),
   y_pred = as.numeric(randomGIB$exactATE)
 ), 4)
-
-
-
-
